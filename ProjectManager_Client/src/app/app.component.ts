e90b9a9abfc31b60271fe47229a6b10cc0bc4c0b
santoshmohan04 <santosh_tagore04@rediffmail.com>
2025-04-09 10:23:16 +0530

    Fixed Changes

--- ProjectManager_Client/src/app/app.component.ts
+++ ProjectManager_Client/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
+import { HeaderComponent } from './project/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [NgbNavModule, RouterModule],
+  imports: [NgbNavModule, RouterModule, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
