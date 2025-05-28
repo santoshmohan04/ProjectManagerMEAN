c2d6014ae9628c39da15dff6764c18cf903f4464
santoshmohan04 <santosh_tagore04@rediffmail.com>
2025-05-28 12:19:33 +0530

    Header fixed UI for Responsiveness

--- ProjectManager_Client/src/app/shared/header/header.component.ts
+++ ProjectManager_Client/src/app/shared/header/header.component.ts
@@ -1,16 +1,33 @@
-import { Component } from '@angular/core';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { Component, HostListener, OnInit } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterModule } from '@angular/router';
 
 @Component({
-    selector: 'app-header',
-    standalone: true,
-    imports: [MatToolbarModule, MatButtonModule, MatIconModule, RouterModule],
-    templateUrl: './header.component.html',
-    styleUrl: './header.component.scss'
+  selector: 'app-header',
+  standalone: true,
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, RouterModule],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss',
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  isMobile = false;
+  menuOpen = false;
 
+  @HostListener('window:resize', [])
+  onWindowResize() {
+    this.isMobile = window.innerWidth <= 768;
+    if (!this.isMobile) {
+      this.menuOpen = false;
+    }
+  }
+
+  ngOnInit() {
+    this.onWindowResize();
+  }
+
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
 }
