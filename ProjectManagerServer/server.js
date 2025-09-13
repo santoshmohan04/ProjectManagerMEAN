24d080e8416c0180753e01631af47900793b2f50
santosh mohan <santosh_tagore04@rediffmail.com>
2025-09-13 23:21:02 +0530

    Refactor styles.scss for improved readability and consistency
    - Updated Bootstrap SCSS import to use a namespace.
    - Reformatted CSS rules for better alignment and consistency.
    - Grouped related styles together for clarity.
    - Ensured consistent use of spacing and indentation throughout the file.

--- ProjectManagerServer/server.js
+++ ProjectManagerServer/server.js
@@ -1,12 +1,12 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import cors from 'cors';
-import mongoose from 'mongoose';
-import DBconfig from './config/ProjectManagerDB.js';
-import userController from './controllers/user.controller.js';
-import projectController from './controllers/project.controller.js';
-import parentTaskController from './controllers/parentTask.controller.js';
-import taskController from './controllers/task.controller.js';
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import mongoose from "mongoose";
+import DBconfig from "./config/ProjectManagerDB.js";
+import userController from "./controllers/user.controller.js";
+import projectController from "./controllers/project.controller.js";
+import parentTaskController from "./controllers/parentTask.controller.js";
+import taskController from "./controllers/task.controller.js";
 
 const app = express();
 const port = process.env.PORT || 4300;
@@ -14,24 +14,23 @@ const port = process.env.PORT || 4300;
 app.use(cors());
 app.use(bodyParser.json());
 
-console.log(DBconfig);
-
 mongoose.Promise = global.Promise;
 
-mongoose.connect(DBconfig.ConnectionString)
+mongoose
+  .connect(DBconfig.ConnectionString)
   .then(() => {
-    console.log('ProjectManager Database is connected');
+    console.log("ProjectManager Database is connected");
   })
   .catch((err) => {
-    console.error('Cannot connect to the ProjectManager database:', err);
+    console.error("Cannot connect to the ProjectManager database:", err);
   });
 
-mongoose.set('debug', true);
+mongoose.set("debug", true);
 
-app.use('/users', userController);
-app.use('/projects', projectController);
-app.use('/parenttasks', parentTaskController);
-app.use('/tasks', taskController);
+app.use("/users", userController);
+app.use("/projects", projectController);
+app.use("/parenttasks", parentTaskController);
+app.use("/tasks", taskController);
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
