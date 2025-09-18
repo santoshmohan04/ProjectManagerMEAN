5024bd3ef6248f98276b13a20d9fb05893dc8fa5
santosh mohan <66831164+santoshmohan04@users.noreply.github.com>
2025-09-18 11:39:05 +0000

    Initiated Task component

--- ProjectManager_Client/src/app/task/components/tasklist/tasklist.component.ts
+++ ProjectManager_Client/src/app/task/components/tasklist/tasklist.component.ts
@@ -1,11 +1,77 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  AfterViewInit,
+  Component,
+  OnDestroy,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatSort, MatSortModule } from '@angular/material/sort';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { Project } from '../../../project/models/project';
+import { ProjectService } from '../../../project/services/project.service';
+import { AlertService } from '../../../shared/services/alert.service';
+import { Subject, takeUntil } from 'rxjs';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-tasklist',
-  imports: [],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatTableModule,
+    MatSortModule,
+    MatPaginatorModule,
+    MatIconModule,
+    MatButtonModule,
+    MatDialogModule,
+  ],
   templateUrl: './tasklist.component.html',
-  styleUrl: './tasklist.component.scss'
+  styleUrl: './tasklist.component.scss',
 })
-export class TasklistComponent {
+export class TasklistComponent implements OnInit, AfterViewInit, OnDestroy {
+  displayedColumns: string[] = [
+    'id',
+    'task',
+    'parent',
+    'priority',
+    'startDate',
+    'endDate',
+    'status',
+    'actions',
+  ];
+  dataSource!: MatTableDataSource<Project>;
+  selectedproject = new FormControl('');
+  destroy$: Subject<boolean> = new Subject<boolean>();
 
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  constructor(
+    private readonly projectService: ProjectService,
+    private readonly alertService: AlertService,
+    private readonly dialogService: MatDialog
+  ) {}
+
+  ngOnInit(): void {}
+
+  ngAfterViewInit() {
+    if (this.dataSource) {
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next(true);
+    this.destroy$.unsubscribe();
+  }
 }
